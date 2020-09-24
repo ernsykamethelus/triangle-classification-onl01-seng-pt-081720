1e58019a7f9adfd607ebf734038f306c11dec41d
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-24 00:17:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,12 @@
 class Triangle
   @sides = []
 
-  def initialize(side1, side2, side3)
-    @side1, @side2, @side3 = side1, side2, side3
-    @sides = [side1, side2, side3]
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
+
 
   def kind
     if(negative? == true || valid? == false)
