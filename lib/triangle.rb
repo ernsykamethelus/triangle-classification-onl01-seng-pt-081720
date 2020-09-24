efbbefff13133c7d4ef50f2f62788b9db35dfc0e
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-24 00:36:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,51 +1,31 @@
 class Triangle
-  @sides = []
-
-  def initialize(side_1,side_2,side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
-
   def kind
-    check_triangle
-      raise TriangleError
-    elsif
+    validate_triangle
+    if a == b && b == c
       :equilateral
-    elsif
-      :scalene
-    elsif
+    elsif a == b || b == c || a == c
       :isosceles
+    else
+      :scalene
     end
   end
 
-    def check_triangle
-   real_triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)]
-    [side_1, side_2, side_3].each do |side|
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
       real_triangle << false if side <= 0 
     raise TriangleError if real_triangle.include?(false)
     end
   end
 
-  def equilateral?
-    (@side1 == @side2) && (@side1 == @side3)
+  class TriangleError < StandardError
   end
-
-  def scalene?
-    (@side1 != @side2) && (@side1!= @side3) && (@side2 != @side3)
-  end
-
-  def isosceles?
-    (@side2 == @side3) || (@side1 == @side2) || (@side1 = @side3)
-  end
-
-  def valid?
-    (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
-  end
-
-end
-
-class TriangleError < StandardError
 
 end
